2f513558a354803ab79396ccfe5bd808a4083798
Vyacheslav <guzxaroh@gmail.com>
2024-09-24 22:56:26 +0700

    added first homework

---
+++ HomeWork#1/HomeWork#1/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HomeWork#1
+//
+//  Created by Vyacheslav Gusev on 24.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
