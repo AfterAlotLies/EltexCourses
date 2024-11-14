033c835390ebc9743cee53284d78b97a1805c26d
Vyacheslav <guzxaroh@gmail.com>
2024-11-15 02:21:21 +0700

    added HomeWork#7 - MVVM + Combine + UIKit

---
+++ HomeWork#7/HomeWork#7/Application/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  HomeWork#7
+//
+//  Created by Vyacheslav Gusev on 14.11.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
