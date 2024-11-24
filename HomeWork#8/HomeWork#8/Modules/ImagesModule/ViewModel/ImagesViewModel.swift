5bd871b131cd3c1315c5fe966f0faa3fd1858e24
Vyacheslav <guzxaroh@gmail.com>
2024-11-24 17:37:43 +0700

    added some refactoring to exist code

--- HomeWork#8/HomeWork#8/Modules/ImagesModule/ViewModel/ImagesViewModel.swift
+++ HomeWork#8/HomeWork#8/Modules/ImagesModule/ViewModel/ImagesViewModel.swift
@@ -10,27 +10,18 @@ import Foundation
 final class ImagesViewModel {
     
     private let imagesURLs: [String] = [
-//        "https://www.cnet.com/a/img/resize/763a63f8451291f93958420b4b9fc2ba69ed57e5/hub/2024/11/19/dbd88023-5beb-4cd8-95f1-12bc6ca43740/screenshot-2024-11-19-at-1-32-56pm.png?auto=webp&fit=crop&height=675&width=1200",
-//        "https://media.wired.com/photos/66ea077283cd4f2fbb17d478/191:100/w_1280,c_limit/WIRED-Coupons-2.jpg",
-//        "https://images.macrumors.com/t/u419F9T-2zXWL5Hkvaw4AqK7ZBg=/2500x/article-new/2024/10/Generic-iOS-18.2-Feature-Real-Mock.jpg",
-//        "https://www.cnet.com/a/img/resize/5e32652c9083dc1d09521e0787fe58eb604b81a7/hub/2024/09/17/7f399551-e39c-46a9-b6f5-ee2efae5e89a/apple-iphone-16-pro-max-camera-lens-photography-4742.jpg?auto=webp&fit=crop&height=675&width=1200",
-//        "https://www.cnet.com/a/img/resize/fa85b07ee746dc2d7be10cf243f0d5d1ecb3666e/hub/2024/11/15/01b6bcaa-e7cb-4066-8d10-167cb790dfe2/survey.jpg?auto=webp&fit=crop&height=675&width=1200",
-//        "https://i.insider.com/673dd3a4ede4eeae39288ee5?width=1200&format=jpeg",
-//        "https://www.cnet.com/a/img/resize/c44672906aee2a0bfaf39bba01791b7bf5a33572/hub/2024/11/20/fc18aa42-005a-4254-9467-4d57d4957cf4/magsafe-black-friday.png?auto=webp&fit=crop&height=675&width=1200",
-        "https://upload.wikimedia.org/wikipedia/commons/6/66/Mt-St-Greg-RueEcureuils-3.jpg",
-                "https://upload.wikimedia.org/wikipedia/commons/a/af/Tatra_Gebirge%2C_Wald_bei_Schnee.jpg",
-                "https://moya-planeta.ru/upload/images/xl/dd/75/dd7517a07597e867f71d31fc47b19e192340b4cd.jpg",
-                "https://i.pinimg.com/736x/15/2f/08/152f08ae907bac5d4459990821b4d4c8.jpg",
-                "https://ic.pics.livejournal.com/helen_i_rebyata/69641658/256815/256815_original.jpg",
-                "https://mgazeta.com/attachments/5038d916aed6361224fdc3609f0c932d42416598/store/crop/0/0/1200/750/1600/0/0/cc69271782ef859a3f549d162c66196fcd2c2b3e3b1dffba40b6928af75d/9047978bebce89cc84514a53b26ed672.jpg",
-                "https://img.desktopwallpapers.ru/nature/pics/wide/1920x1080/f403f1d9961eaf6205e778f4b52ca570.jpg",
-                "https://topoboi.vip/wallpaper/thumb1000/main1000_spring_3840x2400_p48fn.jpg"
-
+        "https://www.cnet.com/a/img/resize/763a63f8451291f93958420b4b9fc2ba69ed57e5/hub/2024/11/19/dbd88023-5beb-4cd8-95f1-12bc6ca43740/screenshot-2024-11-19-at-1-32-56pm.png?auto=webp&fit=crop&height=675&width=1200",
+        "https://media.wired.com/photos/66ea077283cd4f2fbb17d478/191:100/w_1280,c_limit/WIRED-Coupons-2.jpg",
+        "https://images.macrumors.com/t/u419F9T-2zXWL5Hkvaw4AqK7ZBg=/2500x/article-new/2024/10/Generic-iOS-18.2-Feature-Real-Mock.jpg",
+        "https://www.cnet.com/a/img/resize/5e32652c9083dc1d09521e0787fe58eb604b81a7/hub/2024/09/17/7f399551-e39c-46a9-b6f5-ee2efae5e89a/apple-iphone-16-pro-max-camera-lens-photography-4742.jpg?auto=webp&fit=crop&height=675&width=1200",
+        "https://www.cnet.com/a/img/resize/fa85b07ee746dc2d7be10cf243f0d5d1ecb3666e/hub/2024/11/15/01b6bcaa-e7cb-4066-8d10-167cb790dfe2/survey.jpg?auto=webp&fit=crop&height=675&width=1200",
+        "https://i.insider.com/673dd3a4ede4eeae39288ee5?width=1200&format=jpeg",
+        "https://www.cnet.com/a/img/resize/c44672906aee2a0bfaf39bba01791b7bf5a33572/hub/2024/11/20/fc18aa42-005a-4254-9467-4d57d4957cf4/magsafe-black-friday.png?auto=webp&fit=crop&height=675&width=1200",
     ]
     
     private let options: [DownloadOptions] = [ .circle,
-//                                               .cached(.memory),
-                                               /*.resize*/]
+                                               .cached(.memory),
+                                               .resize]
     
     var onLoadImages: ((String, [DownloadOptions]) -> Void)?
     
