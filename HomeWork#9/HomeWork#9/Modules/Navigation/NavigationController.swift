d35db2ab311eebcbc8459b0b70e5c07343eae00f
Vyacheslav <guzxaroh@gmail.com>
2024-11-29 20:07:20 +0700

    added Navigation for app

---
+++ HomeWork#9/HomeWork#9/Modules/Navigation/NavigationController.swift
@@ -0,0 +1,27 @@
+//
+//  NavigationController.swift
+//  HomeWork#9
+//
+//  Created by Vyacheslav Gusev on 27.11.2024.
+//
+
+import UIKit
+
+final class NavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigation()
+    }
+}
+
+private extension NavigationController {
+    
+    func setupNavigation() {
+        let networkManager = ImagesListNetworkService()
+        let imagesViewModel = ImagesListViewModel(networkManager: networkManager)
+        let imageCellViewModel = ImageCellViewModel(networkManager: networkManager)
+        let imagesListController = ImagesViewController(viewModel: imagesViewModel, imageCellViewModel: imageCellViewModel)
+        viewControllers = [imagesListController]
+    }
+}
