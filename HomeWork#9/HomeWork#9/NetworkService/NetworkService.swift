ac21865218f1438dd8742b767d7b7e2149fbf6e8
Vyacheslav <guzxaroh@gmail.com>
2024-11-30 04:28:44 +0700

    changed NetworkService

--- HomeWork#9/HomeWork#9/NetworkService/NetworkService.swift
+++ HomeWork#9/HomeWork#9/NetworkService/NetworkService.swift
@@ -1,5 +1,5 @@
 //
-//  ImagesListNetworkService.swift
+//  NetworkService.swift
 //  HomeWork#9
 //
 //  Created by Vyacheslav Gusev on 28.11.2024.
@@ -8,20 +8,15 @@
 import Foundation
 import Combine
 
-protocol IImagesListNetworkService {
-    func fetchImagesData<T: Codable>(model: T.Type) -> AnyPublisher<T, Error>
-    func fetchImage(imageURL: String) -> AnyPublisher<Data, Error>
-}
-
-final class ImagesListNetworkService: NSObject, IImagesListNetworkService {
+final class NetworkService: NSObject, ImagesListNetworkProtocol, MediaUploadNetworkProtocol {
     
     enum NetworkErrors: Error {
         case invalidURL
         case invalidData
     }
     
-    private(set) var downloadProgressPublisher = PassthroughSubject<Float, Never>()
     private var downloadImagePromise: ((Result<Data, Error>) -> Void)?
+    var downloadProgressPublisher: PassthroughSubject<Float, Never> = PassthroughSubject<Float, Never>()
     
     func fetchImagesData<T: Codable>(model: T.Type) -> AnyPublisher<T, Error> {
         guard let request = createRequest(with: "http://164.90.163.215:1337/api/upload/files", for: "GET") else {
@@ -55,9 +50,21 @@ final class ImagesListNetworkService: NSObject, IImagesListNetworkService {
         .receive(on: DispatchQueue.main)
         .eraseToAnyPublisher()
     }
+    
+    func getImageFromUrl(from url: String) -> AnyPublisher<Data, Error> {
+        guard let request = createRequest(with: url, for: "GET") else {
+            return Fail(error: NetworkErrors.invalidURL)
+                .eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .tryMap { $0.data }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
 }
 
-extension ImagesListNetworkService: URLSessionDownloadDelegate {
+extension NetworkService: URLSessionDownloadDelegate {
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
         let progress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
@@ -74,7 +81,7 @@ extension ImagesListNetworkService: URLSessionDownloadDelegate {
     }
 }
 
-private extension ImagesListNetworkService {
+private extension NetworkService {
     
     func createRequest(with url: String, for httpMethod: String) -> URLRequest? {
         guard let url = URL(string: url) else { return nil }
